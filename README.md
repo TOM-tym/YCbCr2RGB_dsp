852bc3abd5af19f01230b405f2c40c4b29467503
TOM-tym <534624022@qq.com>
2020-12-20 11:09:02 +0800

    Program is functional, need to be optimized,

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # YCbCr2RGB_dsp
+
+### Author: Ouwzhong, TOM-tym.
+
+#### Abstract
+
+This is a implementation of YCbCr to RGB color space conversion, coding for TMS320C6000 series DSPs from Texas Instruments.
+
+Linear assembly language is used.
+
+This implementation is one part of homework of course `Principles and Applications of DSP Devices` , SEIT, SYSU.
+
+#### Useage:
+
+1. Define your YCbCr data in `main.c`, modify variables `YCbCr`.
+2. RUN THE PROGRAM!
+3. (optional) Since we've only got integer that is  32-bit long in C64xx DSPs, for fix-point numbers, we actually define the length of decimal part is 20 (variable `pos`). You can enlarge it if you want more precise numbers. Note that pixel values are ranked form 0 to 255 (8-bits), please make sure that integer part is longer than 8+1 bits (1 for sign flag), i.e.  $pos<=(32-1-8)=21$.
+
+#### Update log:
+
+- 2020.19.20 Program is functional, but we just calling c-style sub-function at the moment (_multipy32), may cause performance loss.
+
+- 2020.12.19 Add a framework.
--- main.c
+++ main.c
@@ -2,27 +2,38 @@
 #include <math.h>
 
 extern int multiply32(int a, int b, int pos);
-extern int YCbCr2RGB(int a, int b, int c, int pos);
+extern void YCbCr2RGB(int* YCbCr, int* RGB, int pos);
 float show_fix_point(int, int);
 int main(){
-	int res = YCbCr2RGB(1,2,3,20);
-	float res_S = show_fix_point(res,20);
-	printf("%f",res_S);
+	test_main();
+	int YCbCr[] = {17,132,127};
+	int RGB[] = {0,0,0};
+	int pos = 20;
+	YCbCr2RGB(YCbCr,RGB,pos);
+	// float res_Y = show_fix_point(YCbCr[0],20);
+	// float res_Cb = show_fix_point(YCbCr[1],20);
+	// float res_Cr = show_fix_point(YCbCr[2],20);
+	printf("[%d,%d,%d]",YCbCr[0],YCbCr[1],YCbCr[2]);
+	// float res_R = show_fix_point(RGB[0],20);
+	// float res_G = show_fix_point(RGB[1],20);
+	// float res_B = show_fix_point(RGB[2],20);
+	printf("[%d,%d,%d]",RGB[0],RGB[1],RGB[2]);
+
 	return 0;
 }
 int test_main(){
-	int t = -4;
-	int dividend[] = {0x00129FBE,0x00129FBE,0xFFFD8000,0xFFFA7EFA,0x00641000,0x00010000,0x02000000};
-	int divisor[] = {0x00020000,0x7FF29FAE,0x000092A0,0xFFFD8000,0x00021000,0x00030000,0x03000000};
+	int dividend[] = {0x01100000,0xFFF00000,0x00400000,0xFFF00000,0x00400000};
+	int divisor[] = {0x00129FBE,0x00166E97,0xFFFA7EFA,0xFFF49375,0x001C5A1c};
 	int i = 0;
 	int pos = 20;
-	for (;i<7;i++){
+	for (;i<5;i++){
 		int a = dividend[i];
 		int b = divisor[i];
 		int res = multiply32(a,b,pos);
 		float s_a = show_fix_point(a,pos);
 		float s_b = show_fix_point(b,pos);
 		float s_res = show_fix_point(res,pos);
+		printf("%x * %x = %x  ",a,b,res);
 		printf("%f * %f = %f\n",s_a,s_b,s_res);
 
 	}return 0;
